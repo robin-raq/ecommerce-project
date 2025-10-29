40dcecebe7a25f033bcfea8e244ecb28f4094575
robin-raq <raqdrobinson@gmail.com>
2025-10-29 16:08:56 -0400

    convert orders page to use async await

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -8,9 +8,11 @@ import { formatMoney } from "../../utils/money";
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
   useEffect(() => {
-    axios.get("/api/orders?expand=products").then((response) => {
+    const fetchOrdersData = async () => {
+      const response = await axios.get("/api/orders?expand=products");
       setOrders(response.data);
-    });
+    };
+    fetchOrdersData();
   }, []);
   return (
     <>
