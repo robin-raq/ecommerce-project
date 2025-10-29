3e084ced788fdd440ea941636060a3e6c2a23fc5
robin-raq <raqdrobinson@gmail.com>
2025-10-28 20:15:33 -0400

    create a products grid component

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import "./HomePage.css";
+import { ProductsGrid } from "./ProductsGrid";
+
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      // console.log(response);
+      setProducts(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <Header cart={cart} />
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
