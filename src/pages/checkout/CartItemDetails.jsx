4d027c26da673a4dd6ea67fd61c640de31fd7cdb
robin-raq <raqdrobinson@gmail.com>
2025-11-04 15:42:54 -0500

    add update quantity input and toggle

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 export function CartItemDetails({ cartItem, fetchCartData }) {
+  const [updateStatus, setUpdateStatus] = useState(false);
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await fetchCartData();
+  };
+  const updateQuantity = function () {
+    setUpdateStatus(!updateStatus);
   };
   return (
     <>
@@ -16,10 +21,19 @@ export function CartItemDetails({ cartItem, fetchCartData }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            {updateStatus ? (
+              <input type="text" className="quantity-textbox" />
+            ) : (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateQuantity}
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
